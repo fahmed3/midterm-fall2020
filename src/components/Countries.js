b7efc4d6bd4fddf24bf219967c3e24dad9cc76d8
Fabiha Ahmed <fsafabiha@gmail.com>
2020-10-23 07:01:55 -0400

    working map, links to all countries

---
+++ src/components/Countries.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { places } from "./data";
+
+function Countries() {
+  return (
+    <div className="Countries">
+      {
+        //Figure out how to list countries
+      }
+    </div>
+  );
+}
+
+export default Countries;
